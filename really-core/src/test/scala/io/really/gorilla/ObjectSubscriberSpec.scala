75bcf9f079f4368173a9cb4590caecac5f374ce5
Hatem AlSum <halsum@cloud9ers.com>
2014-12-15 11:19:21 +0200

    REALLY-64 Add ReallyConfig to BaseActorSpecWithMongo

--- really-core/src/test/scala/io/really/gorilla/ObjectSubscriberSpec.scala
+++ really-core/src/test/scala/io/really/gorilla/ObjectSubscriberSpec.scala
@@ -4,10 +4,13 @@
 
 package io.really.gorilla
 
-import akka.actor.{Terminated, Props, ActorSystem}
-import akka.testkit.{TestProbe, TestActorRef}
+import akka.actor.{ Terminated, Props, ActorSystem }
+import akka.persistence.{ Update => PersistenceUpdate }
+import com.typesafe.config.ConfigFactory
+import play.api.libs.json.Json
+import akka.testkit.{ EventFilter, TestProbe, TestActorRef }
 import _root_.io.really._
-import _root_.io.really.gorilla.SubscriptionManager.{UpdateSubscriptionFields, Unsubscribe}
+import _root_.io.really.gorilla.SubscriptionManager.{ UpdateSubscriptionFields, Unsubscribe }
 import _root_.io.really.protocol.ProtocolFormats.PushMessageWrites.Deleted
 import _root_.io.really.gorilla.SubscribeAggregator.Subscribed
 import _root_.io.really.gorilla.GorillaEventCenter._
@@ -15,12 +18,17 @@ import _root_.io.really.fixture.PersistentModelStoreFixture
 import _root_.io.really.model.Model
 import _root_.io.really.model.persistent.ModelRegistry.CollectionActorMessage
 import _root_.io.really.model.persistent.ModelRegistry.ModelResult
+import _root_.io.really.model.CollectionActor.Event.Created
 import _root_.io.really.model.persistent.PersistentModelStore
-import akka.persistence.{Update => PersistenceUpdate}
 
 import scala.slick.driver.H2Driver.simple._
 
-class ObjectSubscriberSpec extends BaseActorSpecWithMongoDB {
+class ObjectSubscriberSpec(config: ReallyConfig) extends BaseActorSpecWithMongoDB(config) {
+
+  def this() = this(new ReallyConfig(ConfigFactory.parseString("""
+    really.core.akka.loggers = ["akka.testkit.TestEventListener"],
+    really.core.akka.loglevel = WARNING
+                                                                """).withFallback(TestConf.getConfig().getRawConfig)))
 
   val caller = TestProbe()
   val requestDelegate = TestProbe()
@@ -48,11 +56,14 @@ class ObjectSubscriberSpec extends BaseActorSpecWithMongoDB {
 
   "Object Subscriber" should "Initialized Successfully" in {
     val rSub1 = RSubscription(ctx, r, Some(Set("name")), rev, requestDelegate.ref, pushChannel.ref)
-    val objectSubscriberActor = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub1))
-    objectSubscriberActor.underlyingActor.fields shouldEqual Set("name")
-    //TODO Check for R
-    //TODO Check for logTag
-    //TODO Check for subscribing on emty lists
+    val objectSubscriber = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub1)).underlyingActor
+    objectSubscriber.fields shouldEqual Set("name")
+    objectSubscriber.r shouldEqual rSub1.r
+    objectSubscriber.logTag shouldEqual s"ObjectSubscriber ${rSub1.pushChannel.path}$$$r"
+
+    val rSub2 = RSubscription(ctx, r, None, rev, requestDelegate.ref, pushChannel.ref)
+    val objectSubscriber2 = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub2))
+    objectSubscriber2.underlyingActor.fields.size shouldEqual 0
   }
 
   it should "handle Unsubscribe successfully during starter receiver and self termination" in {
@@ -65,14 +76,25 @@ class ObjectSubscriberSpec extends BaseActorSpecWithMongoDB {
     deathPrope.expectTerminated(objectSubscriberActor)
   }
 
-  it should "handle Unsubscribe during withModel and self termination" in {}
+  it should "handle Unsubscribe during withModel and self termination" in {
+    val rSub1 = RSubscription(ctx, r, None, rev, requestDelegate.ref, pushChannel.ref)
+    val objectSubscriberActor = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub1))
+    deathPrope.watch(objectSubscriberActor)
+    val replayer = system.actorOf(Props(new Replayer(globals, objectSubscriberActor, rSub1, Some(rev))))
+    objectSubscriberActor ! ReplayerSubscribed(replayer)
+    Thread.sleep(1000)
+    objectSubscriberActor.underlyingActor.fields.size shouldEqual 2
+    objectSubscriberActor.tell(SubscriptionManager.Unsubscribe, caller.ref)
+    requestDelegate.expectMsg(SubscriptionManager.Unsubscribe)
+    deathPrope.expectTerminated(objectSubscriberActor)
+  }
 
   it should "update Internal field List Successfully" in {
     val rSub1 = RSubscription(ctx, r, Some(Set("name")), rev, requestDelegate.ref, pushChannel.ref)
     val objectSubscriberActor = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub1))
     val replayer = system.actorOf(Props(new Replayer(globals, objectSubscriberActor, rSub1, Some(rev))))
-    objectSubscriberActor.tell(ReplayerSubscribed(replayer), caller.ref)
-    objectSubscriberActor.tell(UpdateSubscriptionFields(Set("age")), caller.ref)
+    objectSubscriberActor ! ReplayerSubscribed(replayer)
+    objectSubscriberActor ! UpdateSubscriptionFields(Set("age"))
     objectSubscriberActor.underlyingActor.fields shouldEqual Set("name", "age")
   }
 
@@ -89,24 +111,43 @@ class ObjectSubscriberSpec extends BaseActorSpecWithMongoDB {
     deathPrope.expectTerminated(objectSubscriberActor)
   }
 
-  it should "for empty list subscription change the internal state from empty list to all model fields" in {}
+  it should "for empty list subscription change the internal state from empty list to all model fields" in {
+    val rSub2 = RSubscription(ctx, r, None, rev, requestDelegate.ref, pushChannel.ref)
+    val objectSubscriber2 = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub2))
+    val replayer = system.actorOf(Props(new Replayer(globals, objectSubscriber2, rSub2, Some(rev))))
+    objectSubscriber2.tell(ReplayerSubscribed(replayer), caller.ref)
+    Thread.sleep(3000)
+    objectSubscriber2.underlyingActor.fields shouldEqual Set("name", "age")
+  }
 
-  it should "log a warning when receiving a create push update" in {}
+  it should "log a warning when receiving a create push update" in {
+    val rSub1 = RSubscription(ctx, r, None, rev, requestDelegate.ref, pushChannel.ref)
+    val objectSubscriberActor = TestActorRef[ObjectSubscriber](globals.objectSubscriberProps(rSub1))
+    deathPrope.watch(objectSubscriberActor)
+    val replayer = system.actorOf(Props(new Replayer(globals, objectSubscriberActor, rSub1, Some(rev))))
+    objectSubscriberActor ! ReplayerSubscribed(replayer)
+    val userObject = Json.obj("_r" -> r.toString, "_rev" -> 1L, "name" -> "Hatem", "age" -> 30)
+    val createdEvent = GorillaLogCreatedEntry(r, userObject, 1L, 1L, ctx.auth)
+    //Test unexpected behaviour
+    EventFilter.warning(message = s"Object Subscriber got an `Created` event which doesn't make any sense!: $createdEvent") intercept {
+      objectSubscriberActor ! createdEvent
+    }
+  }
 
-  it should "filter the hidden fields from the an empty list subscription and sent the rest of model fields" in {}
+  ignore should "filter the hidden fields from the an empty list subscription and sent the rest of model fields" in {}
 
-  it should "filter the hidden fields from the the subscription list and sent the rest of the subscription list " in {}
+  ignore should "filter the hidden fields from the the subscription list and sent the rest of the subscription list " in {}
 
-  it should "pass nothing if the model.executeOnGet evaluated to Terminated" in {}
+  ignore should "pass nothing if the model.executeOnGet evaluated to Terminated" in {}
 
-  it should "in case of subscription failure, log and acknowledge the delegate and then stop" in {}
+  ignore should "in case of subscription failure, log and acknowledge the delegate and then stop" in {}
 
-  it should "handle associated replayer termination" in {}
+  ignore should "handle associated replayer termination" in {}
 
-  it should "handle ModelUpdated correctly" in {}
+  ignore should "handle ModelUpdated correctly" in {}
 
-  it should "handle ModelDeleted, send subscription failed and terminates" in {}
+  ignore should "handle ModelDeleted, send subscription failed and terminates" in {}
 
-  it should "handle if the pushed update model version is not equal to the state version" in {}
+  ignore should "handle if the pushed update model version is not equal to the state version" in {}
 
 }
